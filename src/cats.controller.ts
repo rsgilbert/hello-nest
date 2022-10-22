eee8f8a518d0d3e6e3fc48d586bd0d475d8e07bb
Gilbert <gssenyonjo@gmail.com>
2022-10-22 15:14:59 +0300

    Looked at providers and created CatsService

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,18 +1,21 @@
 import { Controller, Get, Post, Req, Body, HttpCode, Redirect, Param } from '@nestjs/common'
 import { Request } from 'express'
 import { CreateCatDto } from './dto/create-cat.dto'
+import { Cat } from './interfaces/cat.interface'
+import { CatsService } from './services/cats.service'
 
 @Controller('cats')
 export class CatsController {
+    constructor(private catsService: CatsService) {}
+
     @Get()
-    findAll(@Req() request: Request): string {
-        return 'Here are all the cats'
+    async findAll(@Req() request: Request): Promise<Cat[]> {
+        return this.catsService.findAll()
     }
 
-    @HttpCode(203)
     @Post()
-    create(@Body() createCatDto: CreateCatDto) : string {
-        return 'Cat created. It is' + JSON.stringify(createCatDto)
+    create(@Body() createCatDto: CreateCatDto) : void {
+        this.catsService.create(createCatDto)
     }
 
     @Get("/redir")
